911ccb7e54ce1ca43f02d4fced6a3349ab36f11e
landbuoy <maxcoker97@gmail.com>
2023-02-20 03:47:59 +0400

    feb 20 update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,14 +55,7 @@ $(document).ready(function () {
     $(".modalContainer--uploadIIThoughts").fadeOut();
   });  
 
-// newgraphic glyph
-  $(".glyph--newGraphic").click(function () {
-    $(".modalContainer").css("zIndex", "0");
-    $(".modalContainer--newGraphic").fadeIn().css("zIndex", "9999");
-  });
-  $(".aquaButton--newGraphic").click(function () {
-    $(".modalContainer--newGraphic").fadeOut();
-  }); 
+
 
   // color stuff
 
@@ -121,46 +114,202 @@ $(document).ready(function () {
     $(".landBuoyContainer1").css("top", navBoxHeight + 32 +"px");
     
 });
+// new graphic
+var musicNewGraphicLeft = [
+  "assets/audio/newGraphic001/birdland.mp3",
+  "assets/audio/newGraphic001/semimelodic.mp3",
+  "assets/audio/newGraphic001/semimelodic2.mp3",
+  "assets/audio/newGraphic001/keys.mp3",
+];
+
+var musicNewGraphicRight = [
+  "assets/audio/newGraphic001/fax.mp3",
+  "assets/audio/newGraphic001/fax2.mp3",
+  "assets/audio/newGraphic001/percussive.mp3",
+  "assets/audio/newGraphic001/shrillambient.mp3",
+];
+
+shuffleL(musicNewGraphicLeft);
+shuffleR(musicNewGraphicRight);
+
+initMusicNewGraphic();
+
+function initMusicNewGraphic() {
+  currentL = 0;
+  currentR = 0;
+  audioL = $("#audioPlayerNewGraphicLeft");
+  audioR = $("#audioPlayerNewGraphicRight");
+  audioL[0].volume = 0.8;
+  audioR[0].volume = 0.8;
+  lenL = musicNewGraphicLeft.length;
+  lenR = musicNewGraphicRight.length;
+
+  runL(musicNewGraphicLeft[currentL], audioL[0]);
+  runR(musicNewGraphicRight[currentR], audioR[0]);
+  
+
+  audioL[0].addEventListener("ended", function (e) {
+    currentL++;
+    if (currentL == lenL) {
+      currentL = 0;
+    }
+    runL(musicNewGraphicLeft[currentL], audioL[0]);
+  });
+
+  audioR[0].addEventListener("ended", function (e) {
+    currentR++;
+    if (currentR == lenR) {
+      currentR = 0;
+    }
+    runR(musicNewGraphicRight[currentR], audioR[0]);
+  });
+
+  audioL[0].pause();
+  audioR[0].pause();
+
+  //play audio on opening modal, pause on close
+  $(".glyph--newGraphic").click(function () {
+    if (audioL[0].paused == false) {
+      audioL[0].pause();
+      audioR[0].pause();
+      //$(".mainLogo").removeClass("mainLogo--active");
+    } else {
+      audioL[0].play();
+      audioR[0].play();
+      //$(".mainLogo").addClass("mainLogo--active");
+    }
+    $(".modalContainer").css("zIndex", "0");
+    $(".modalContainer--newGraphic").fadeToggle().css("zIndex", "9999");
+    /*if (audio2[0].paused == false) {
+      audio2[0].pause();
+    } else {
+      // Do nothing
+    }*/
+  });
+
+//stop channel, switch to next track on resume. what happens on imagemap click? different graphic every time too. replace element function?
+
+
+
+}
+
+function runL(link, $audio) {
+  audioL[0].src = link;
+  audioL[0].load();
+}
+function runR(link, $audio) {
+  audioR[0].src = link;
+  audioR[0].load();
+}
+
+function shuffleL(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function shuffleR(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// teapot
+/*const canvas = document.getElementById('teapot');
+      const ctx = canvas.getContext('2d');
+      
+      const teapot = {
+var vertices = [
+  [-0.5, 0, 0.5],    // 0
+  [-0.5, -0.5, 0.5], // 1
+  [0.5, -0.5, 0.5],  // 2
+  [0.5, 0, 0.5],     // 3
+  [-0.5, 0, -0.5],   // 4
+  [-0.5, -0.5, -0.5],// 5
+  [0.5, -0.5, -0.5], // 6
+  [0.5, 0, -0.5],    // 7
+  [-0.25, 0.25, 0],  // 8
+  [-0.25, 0.25, 0],  // 9
+  [-0.25, -0.25, 0], // 10
+  [-0.25, -0.25, 0], // 11
+  [-0.25, 0.25, 0.5],// 12
+  [-0.25, -0.25, 0.5],// 13
+  [0.25, -0.25, 0.5], // 14
+  [0.25, 0.25, 0.5],  // 15
+  [-0.25, 0.25, -0.5],// 16
+  [-0.25, -0.25, -0.5],// 17
+  [0.25, -0.25, -0.5], // 18
+  [0.25, 0.25, -0.5],  // 19
+  [0, 0.35, 0],       // 20
+  [0, 0.35, 0],       // 21
+  [0, 0.35, 0],       // 22
+  [0, 0.35, 0]        // 23
+];
+
+
+          
+        var edges = [
+  [0,1], [1,2], [2,3], [3,0], // rim
+  [4,5], [5,6], [6,7], [7,4], // body
+  [0,4], [1,5], [2,6], [3,7], // legs
+  [0,8], [1,9], [2,10], [3,11], // handle
+  [4,8], [5,9], [6,10], [7,11], // spout
+  [12,13], [13,14], [14,15], [15,12], // lid
+  [16,17], [17,18], [18,19], [19,16], // base
+  [20,21], [21,22], [22,23], [23,20] // comb
+];
+
+        
+      };
+
+      function drawTeapot() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+
+        for (let i = 0; i < teapot.edges.length; i += 2) {
+          const v1 = teapot.vertices[teapot.edges[i]];
+          const v2 = teapot.vertices[teapot.edges[i+1]];
+          ctx.moveTo(v1[0], v1[1]);
+          ctx.lineTo(v2[0], v2[1]);
+        }
+
+        ctx.stroke();
+      }
+
+      // Call drawTeapot() here to draw the teapot initially
+      //drawTeapot();*/
 
 // Player for channel 1
 
-/*var musicarr1 = [
-  "assets/audio/ch1/A1.mp3",
-  "assets/audio/ch1/A2.mp3",
-  "assets/audio/ch1/A3.mp3",
-  "assets/audio/ch1/A4.mp3",
-  "assets/audio/ch1/A5.mp3",
-  "assets/audio/ch1/A6.mp3",
-  "assets/audio/ch1/A7.mp3",
-  "assets/audio/ch1/A8.mp3",
-  "assets/audio/ch1/A9.mp3",
-  "assets/audio/ch1/A10.mp3",
-  "assets/audio/ch1/A11.mp3",
-  "assets/audio/ch1/A12.mp3",
-  "assets/audio/ch1/A13.mp3",
-  "assets/audio/ch1/A14.mp3",
-  "assets/audio/ch1/A15.mp3",
-  "assets/audio/ch1/A16.mp3",
-  "assets/audio/ch1/A17.mp3",
-  "assets/audio/ch1/A18.mp3",
-  "assets/audio/ch1/A19.mp3",
-  "assets/audio/ch1/A20.mp3",
-  "assets/audio/ch1/A21.mp3",
-  "assets/audio/ch1/A22.mp3",
-  "assets/audio/ch1/A23.mp3",
-  "assets/audio/ch1/A24.mp3",
-  "assets/audio/ch1/A25.mp3",
-  "assets/audio/ch1/A26.mp3",
-  "assets/audio/ch1/A27.mp3",
-  "assets/audio/ch1/A28.mp3",
-  "assets/audio/ch1/A29.mp3",
-  "assets/audio/ch1/A30.mp3",
-  "assets/audio/ch1/A31.mp3",
-  "assets/audio/ch1/A32.mp3",
-  "assets/audio/ch1/A33.mp3",
-];
-
-shuffle1(musicarr1);
+/*shuffle1(musicarr1);
 
 init1();
 
