d9dccc548f509fea6087522c6410f07071dc6388
landbuoy <maxcoker97@gmail.com>
2023-06-16 21:21:17 +0300

    new js files per subpage

---
+++ assets/js/hausaufgabe.js
@@ -0,0 +1,26 @@
+function randomColor() {
+        var color;
+        color = Math.floor(Math.random() * 0x1000000); // integer between 0x0 and 0xFFFFFF
+        color = color.toString(16); // convert to hex
+        color = ("000000" + color).slice(-6); // pad with leading zeros
+        color = "#" + color; // prepend #
+        return color;
+    }
+  
+  function invertColor(hexTripletColor) {
+        var color = hexTripletColor;
+        color = color.substring(1); // remove #
+        color = parseInt(color, 16); // convert to integer
+        color = 0xFFFFFF ^ color; // invert three bytes
+        color = color.toString(16); // convert to hex
+        color = ("000000" + color).slice(-6); // pad with leading zeros
+        color = "#" + color; // prepend #
+        return color;
+    }
+
+    var c1 = randomColor();
+
+    var c2 = invertColor(c1);
+
+$(".hausaufgabe").css("background", "linear-gradient(to right, transparent, " + c2 );
+$(".modalContainer--hausaufgabeInfo").css("box-shadow", "0px 0px 32px" + c2);
--- assets/js/moss.js
+++ assets/js/moss.js
@@ -1,3 +1,35 @@
+  function randomColor() {
+        var color;
+        color = Math.floor(Math.random() * 0x1000000); // integer between 0x0 and 0xFFFFFF
+        color = color.toString(16); // convert to hex
+        color = ("000000" + color).slice(-6); // pad with leading zeros
+        color = "#" + color; // prepend #
+        return color;
+    }
+  
+  function invertColor(hexTripletColor) {
+        var color = hexTripletColor;
+        color = color.substring(1); // remove #
+        color = parseInt(color, 16); // convert to integer
+        color = 0xFFFFFF ^ color; // invert three bytes
+        color = color.toString(16); // convert to hex
+        color = ("000000" + color).slice(-6); // pad with leading zeros
+        color = "#" + color; // prepend #
+        return color;
+    }
+
+    var c1 = randomColor();
+
+    var c2 = invertColor(c1);
+
+$(".modalContainer--dancingTriangle").css("box-shadow", "0px 0px 32px" + c2);
+$(".modalContainer--mossComposite").css("box-shadow", "0px 0px 32px" + c2);
+$(".modalContainer--marchComp3-6").css("box-shadow", "0px 0px 32px" + c2);
+$(".modalContainer--marchComp3-8").css("box-shadow", "0px 0px 32px" + c2);
+$(".modalContainer--marchComp3-18").css("box-shadow", "0px 0px 32px" + c2);
+$(".modalContainer--marchComp3-31").css("box-shadow", "0px 0px 32px" + c2);
+$(".mossComposites").css("background", "linear-gradient(to right, transparent, " + c2 );
+
 //dancing triangle scripts
 
 var musicDT = [
